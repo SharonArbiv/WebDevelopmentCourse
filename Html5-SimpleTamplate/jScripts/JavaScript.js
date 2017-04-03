3843a8c1cae35ce0ac47613bd90bca8e23a36d09
SharonArbiv <sharonarbiv2@gmail.com>
2017-04-03 16:17:37 +0300

    Add files via upload

---
+++ Html5-SimpleTamplate/jScripts/JavaScript.js
@@ -0,0 +1,15 @@
+﻿function myFunc()
+{
+    //alert('hello');
+    //var x = 3;
+    //x += 7;
+    //alert(x);
+    // document.getElementById("myDiv").innerHTML=x 
+
+
+    var x = parseFloat(document.getElementById("Text1").value);
+    var y = parseFloat(document.getElementById("Text2").value);
+    var z = parseFloat(document.getElementById("Text3").value);
+    var c = (x + y + z) /3;
+    document.getElementById("myDiv").innerHTML = c;
+}
